40f0d110578f30abef2cbf709092d47ba2e3541b
David Meade Jr <davidmeadejnr@gmail.com>
2022-12-15 13:18:33 +0000

    updated from 10 to 12 tables.

--- 1-java-fundamentals/GalToLitTable.java
+++ 1-java-fundamentals/GalToLitTable.java
@@ -14,7 +14,7 @@ class GalToLitTable {
 		int counter;
 
 		counter = 0;
-		for(inches = 1; inches <= 120; inches++) {
+		for(inches = 1; inches <= 144; inches++) {
 			meters = inches * 39.37; // convert to liters
 			System.out.println(inches + " inches is " + meters + " meters.");
 
