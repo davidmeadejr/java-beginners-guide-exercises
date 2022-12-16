853fa6bddb1ea5435002d74db285759e210426ca
David Meade Jr <davidmeadejnr@gmail.com>
2022-12-16 07:21:05 +0000

    Sound exercise completed.

---
+++ 2-data-types-and-operators/Sound.java
@@ -0,0 +1,14 @@
+/* 
+	Try This 2-1
+	Compute the distance to a lightning 
+	strike whose sound takes 7.2 seconds
+	to reach you.
+*/
+
+class Sound {
+	public static void main(String args[]) {
+		double dist = 7.2 * 1000;
+		System.out.println("The lightning is " + dist + " feet away.");
+	}
+
+}
