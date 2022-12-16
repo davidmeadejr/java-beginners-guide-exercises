6b4f4aff34d28597f09504391edfec39df7f5ad3
David Meade Jr <davidmeadejnr@gmail.com>
2022-12-16 07:27:34 +0000

    Echo exercise completed.

---
+++ 2-data-types-and-operators/Echo.java
@@ -0,0 +1,10 @@
+class Echo {
+	public static void main(String args[]) {
+		double distance;
+		
+		distance = 7.2 / 2;
+
+		System.out.println("The large object is " + distance + " feet away.");
+		
+	}
+}
